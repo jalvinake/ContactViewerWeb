8127d1cadae0cf819f0c4717849a091d146d6cd7
Jake <jake.arntson@gmail.com>
2015-03-17 18:39:35 -0500

    initial commit

---
+++ contact-viewer.js
@@ -0,0 +1,28 @@
+var _contacts = {}
+var _contactid = null;
+
+var _apiKey = 'totally'
+
+$(document).on('pagebeforeshow','#home-page', function(){
+	var contactList = $('#contactlist')
+	$.get('http://contacts.tinyapollo.com/contacts?key=' + _apiKey, 
+	function(result){
+		for(i in result.contacts){
+			var contact = result.contacts[i]
+			_contacts[contact._id] = contact
+			contactList.append('<li><a href="#details-page" data-contact-id="' +contact._id+'"><h3>'+contact.name+'</h3><p>'+contact.title+'</p></a></li>')
+		}
+		contactList.listview('refresh')
+	})
+})
+
+$(document).on('click','#contactlist a', function(){
+	var link = $(this)
+	_contactid = link.data('contact-id')
+	return true
+})
+
+$(document).on('pagebeforeshow','#details-page', function(){
+	var contact = _contacts[_contactid]
+	$('.contact-details').text(contact.name + ' details')
+})
